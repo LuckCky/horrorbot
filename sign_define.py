0c75e06f223b774106fe5fbcd34f8b633bb74f3b
Alex Ivanov <www.aivanov@gmail.com>
2016-11-16 19:39:07 +0300

    sign define started

--- sign_define.py
+++ sign_define.py
@@ -1,14 +1,18 @@
-from datetime import datetime, date
-
 from signs import signs
 from dbhelper import set_user_sign
 
 def sign_define(user_id, birth_date):
-    birth_date = datetime.strptime(birth_date, '%d.%m')#.strftime('%d.%m')
+    if '/' in birth_date:
+        birth_date = birth_date.split('/')
+    elif '.' in birth_date:
+        birth_date = birth_date.split('.')
+    if birth_date == ['29', '2']:
+        return 'Рыбы'
+    birth_date = (int(birth_date[0]), int(birth_date[1]))
     for k in signs:
-        start = datetime.strptime(signs[k][0], '%d.%m')#.strftime('%d.%m')
-        end = datetime.strptime(signs[k][1], '%d.%m')#.strftime('%d.%m')
-        if start <= birth_date <= end:
+        start = signs[k][0]
+        end = signs[k][1]
+        if birth_date <= end:
             # set_user_sign(user_id, k)
             return k
 
