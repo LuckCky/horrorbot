29e8f3f0fbea59e3d01082c0386e3c49a03cadb9
Alex Ivanov <www.aivanov@gmail.com>
2016-11-17 14:40:04 +0300

    sign definition works

--- sign_define.py
+++ sign_define.py
@@ -8,12 +8,17 @@ def sign_define(user_id, birth_date):
         birth_date = birth_date.split('.')
     if birth_date == ['29', '2']:
         return 'Рыбы'
-    birth_date = (int(birth_date[0]), int(birth_date[1]))
-    for k in signs:
-        start = signs[k][0]
-        end = signs[k][1]
-        if birth_date <= end:
-            # set_user_sign(user_id, k)
-            return k
-
-print(sign_define(11, '30.12'))
+    birth_date_day = int(birth_date[0])
+    birth_date_month = int(birth_date[1])
+    birth_date = (birth_date_day, birth_date_month)
+    for num, k in enumerate(signs):
+        start = k[1][0]
+        end_month = k[1][1][1]
+        end_day = k[1][1][0]
+        if birth_date_month == end_month:
+            if birth_date_day <= end_day:
+                # set_user_sign(user_id, signs[num][0])
+                return signs[num][0]
+            else:
+                # set_user_sign(user_id, signs[num+1][0])
+                return signs[num+1][0]
